1178a82a74fb0c32b67b2f2220b23a79fdbc8db3
lucas-trubiano <lucas.trubiano@gmail.com>
2022-06-14 21:26:41 -0300

    final clase 18

--- ProyectoCoder/ProyectoCoderApp/admin.py
+++ ProyectoCoder/ProyectoCoderApp/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
 
-from ProyectoCoderApp.models import Curso
+from ProyectoCoderApp.models import *
 
 # Register your models here.
 class CursoAdmin(admin.ModelAdmin):
 
     list_display = ('nombre', 'comision')
+    search_fields = ('nombre', 'comision')
+
+
+class EstudianteAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'apellido')
+
+
+class ProfesorAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'apellido', 'profesion')
+
+
+class EntregableAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'fechaEntrega', 'entregado')
+
 
 admin.site.register(Curso, CursoAdmin)
+admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Profesor, ProfesorAdmin)
+admin.site.register(Entregable, EntregableAdmin)
 
 # admin, admin -> python manage.py createsuperuser
