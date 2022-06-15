f42cc4cd52f290d4fb3135a6c5cbf0d5e78d9753
lucas-trubiano <lucas.trubiano@gmail.com>
2022-06-14 21:13:26 -0300

    completando proyecto para la clase 18

--- ProyectoCoder/ProyectoCoderApp/models.py
+++ ProyectoCoder/ProyectoCoderApp/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-from numpy import require
 
 # Create your models here.
-class Estudiante(models.Model):
-
-    # id por defecto
-    nombre = models.CharField(max_length=30) # Texto
-    apellido = models.CharField(max_length=30) # Texto
-    email = models.EmailField(blank=True, null=True) # Email - Opcional
-
 
 class Curso(models.Model):
 
     # id por defecto
     nombre = models.CharField(max_length=30) # Texto
     comision = models.IntegerField()
+
+
+class Estudiante(models.Model):
+
+    # id por defecto
+    nombre = models.CharField(max_length=30) # Texto
+    apellido = models.CharField(max_length=30) # Texto
+    email = models.EmailField(blank=True, null=True) # Email - Opcional
 
 
 class Profesor(models.Model):
@@ -26,3 +26,8 @@ class Profesor(models.Model):
 
     profesion = models.CharField(max_length=30)
 
+class Entregable(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    fechaEntrega = models.DateField()
+    entregado = models.BooleanField()
